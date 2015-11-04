13e3d64b4b99d36700b8b7674b1c4ff9db36fc4d
Dylen Brandon Rivera <dylenbrivera@gmail.com>
2015-11-04 01:30:18 +0000

    Added a new login controller and view for user authentication.

---
+++ application/views/login_view.php
@@ -0,0 +1,16 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Login</title>
+</head>
+<body>
+    <h1>Username</h1>
+    <input type="text" name="username" value="" size="50"/>
+    <h1>Password</h1>
+    <input type="text" name="password" value="" size="50"/>
+    <input type="submit" value="Submit"/>
+</body>
+</html>
