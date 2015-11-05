8868fa1287aac5a0abf999f107451ee582b9dbf8
Allen Hai <allenhai03@gmail.com>
2015-11-04 20:40:15 -0600

    removed typo from view method of pages controller

--- application/config/routes.php
+++ application/config/routes.php
@@ -50,4 +50,4 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |		my-controller/my-method	-> my_controller/my_method
 */
 $route['default_controller'] = 'pages/view';
-$route['(:any)'] = 'pages/views/$1';
+$route['(:any)'] = 'pages/view/$1';
