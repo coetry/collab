02afb0d1040001316a781831a298654b383ffa5c
Dylen Brandon Rivera <dylenbrivera@gmail.com>
2015-11-06 04:16:36 +0000

    Updated the Home controller so that it can be used as the default controller. Also updated the login controller to use the same templating as the Home controller.

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -3,10 +3,22 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Home extends CI_Controller {
 
-  public function index()
+  public function view($page = 'home')
   {
+    if (! file_exists(APPPATH.'/views/pages/'.$page.'.php'))
+    {
+      show_404();
+    }
+    
     $data['applicationName'] = 'Code Collaboration';
-    $this->load->view('home_view', $data);
+    $data['title'] = 'Home';
+    
+    $data['welcome_message'] = 'Welcome to Code Collaboration!';
+    
+    $this->load->view('templates/header', $data);
+    $this->load->view('pages/'.$page, $data);
+    $this->load->view('templates/footer', $data);
+    
   }
   
 }
--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -5,7 +5,13 @@ class Login extends CI_Controller {
     
   public function index()
   {
-    $this->load->view('login_view');
+    
+    $data['applicationName'] = 'Code Collaboration';
+    $data['title'] = 'Login';
+    
+    $this->load->view('templates/header', $data);
+    $this->load->view('pages/login', $data);
+    $this->load->view('templates/footer', $data);
   }
   
 }
