084e56b80437aff674a896f732a3333ef48f7f20
Dylen Brandon Rivera <dylenbrivera@gmail.com>
2015-11-06 04:15:07 +0000

    Created a page with a login form.

---
+++ application/views/pages/login.php
@@ -0,0 +1,6 @@
+Username <br/>
+<input type="text" name="username" value="" size="50"/>
+<br/>
+Password <br/>
+<input type="text" name="password" value="" size="50"/>
+<input type="submit" value="Submit"/>
