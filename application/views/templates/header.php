af24377e951a7b5ee15194fb72aafff96d1b9a0b
Dylen Brandon Rivera <dylenbrivera@gmail.com>
2015-11-06 02:03:11 +0000

    Added a login and registration button to the header template.

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -5,6 +5,9 @@
   </head>
   
   <body>
-  
-    <h1></h1>
+    <div class="header">
+      <!-- Logic should be written to determine if these links should be displayed depending on the user session status -->
+      <a href="/login">Login</a> | <a href="/register">Register</a>
+    </div>
+    
     <hr>
